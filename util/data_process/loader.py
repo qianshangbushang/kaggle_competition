c370a994a19924c5e10423e9e129c352a61eda33
qiansahngbushang <5494055779@qq.com>
2019-09-11 00:27:49 +0800

    [commit code] test read csv

--- util/data_process/loader.py
+++ util/data_process/loader.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/9/11 0:10
 # @Author  : chaucerhou
+
+import numpy as np
+
+
+def read_csv(path: str, encoding="utf-8", dtype=str, skiprows=1, delimiter=","):
+    with open(path, encoding=encoding) as f:
+        data = np.loadtxt(f, dtype=dtype, delimiter=delimiter, skiprows=skiprows)
+    return data
